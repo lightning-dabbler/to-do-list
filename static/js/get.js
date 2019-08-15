fad888a2c3a5a014e8318ebf1bc138e4de5d0b26
Osarodion Irabor <33585132+lightning-dabbler@users.noreply.github.com>
2019-08-15 11:45:34 -0400

    disable page transition if selection and endpoint match

--- static/js/get.js
+++ static/js/get.js
@@ -5,28 +5,42 @@
  * 
  * */
 
- // Get History 
+// Get History 
 
 function ajaxGet() {
-    var getRequest = new XMLHttpRequest();
-    var information = document.querySelector('select').value.split(' ')
-    if (information.length==1 || information.length==3){
-        getRequest.open("GET", '/', true)
-        getRequest.onreadystatechange = function () {
-            if(this.readyState === 4 && this.status === 200) {
-              window.location = '/'
-            }
-          }
+  var getRequest = new XMLHttpRequest();
+  var docURI = document.documentURI
+  var endpoint = docURI.split('/')
+  var endpoint = endpoint[endpoint.length - 1].replace(/-/g, '/')
+  console.log(endpoint)
+  var information = document.querySelector('select').value.split(' ')
+  var selectionDate = information[information.length - 1]
+  if (endpoint != selectionDate) {
+    if (endpoint != '' && information.length != 3) {
+      ajaxGetLogic(getRequest, information)
     }
-    else {
+  }
+}
+
+function ajaxGetLogic(getRequest, information) {
+  var information = document.querySelector('select').value.split(' ')
+  if (information.length == 1 || information.length == 3) {
+    getRequest.open("GET", '/', true)
+    getRequest.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        window.location = '/'
+      }
+    }
+  }
+  else {
     let date = information[information.length - 1]
     let url = `/${date.replace(/\//g, '-')}`
     getRequest.open("GET", url, true);
     getRequest.onreadystatechange = function () {
-        if(this.readyState === 4 && this.status === 200) {
-          window.location = `${url}`
-        }
+      if (this.readyState === 4 && this.status === 200) {
+        window.location = `${url}`
       }
     }
-    getRequest.send()
+  }
+  getRequest.send()
 }
