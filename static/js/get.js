572fa2fc0eb4da259f8128978b63a0f15ab1a142
Captain Lightning <[Omitted]>
2019-08-15 23:14:16 -0400

    Select value remains consistent after GET request

--- static/js/get.js
+++ static/js/get.js
@@ -11,20 +11,20 @@ function ajaxGet() {
   var getRequest = new XMLHttpRequest();
   var docURI = document.documentURI
   var endpoint = docURI.split('/')
-  console.log(docURI, endpoint)
+
   var endpoint = endpoint[endpoint.length - 1].replace(/-/g, '/')
-  console.log(endpoint)
+
   var information = document.querySelector('select').value.split(' ')
-  console.log(information)
+
   var selectionDate = information[information.length - 1]
-  console.log(selectionDate)
+
   if (endpoint != selectionDate) {
     if (
       (
-        endpoint != '' && information.length == 3)
-      || (endpoint == '' && information.length != 3) || (
+      endpoint != '' && information.length == 3) 
+      || (endpoint == '' && information.length != 3)||(
         endpoint != '' && information.length != 3)
-    ) {
+      ) {
       ajaxGetLogic(getRequest, information)
     }
   }
